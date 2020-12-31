1c66824038e3bc94ec3d2499e611f1ea2acfd7ed
Joss Lei <825996020@qq.com>
2020-12-31 18:54:09 +0800

    Separate some functions

---
+++ utils.py
@@ -0,0 +1,32 @@
+import torch
+import numpy
+import os
+
+def getStat(datafolder:str, trans=transforms.ToTensor()):
+    traindata = datasets.ImageFolder(datafolder, trans)
+    image_means = torch.stack([t.mean(1).mean(1) for t, c in traindata])
+    image_means.mean(0)
+    return image_means
+    
+def getStat(train_data):
+    print('Compute mean and variance for training data.')
+    print(len(train_data))
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=1, shuffle=False, num_workers=0,
+        pin_memory=True)
+    mean = torch.zeros(1)
+    std = torch.zeros(1)
+    for X, _ in train_loader:
+        mean[0] += X.mean()
+        std[0] += X.std()
+    mean.div_(len(train_data))
+    std.div_(len(train_data))
+    return list(mean.numpy()), list(std.numpy())
+
+def save_loss_values(directory:str, epoch:int, all_loss:list):
+    with open(directory + str(epoch) + "_all", "w") as fp:
+        for i in all_loss:
+            fp.write(str(i) + '\n')
+        fp.flush()
+    return
+
