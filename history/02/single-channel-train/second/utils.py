ba8aaddb5ad3112402e02b4065fcb5ccd67ab50a
Joss Lei <825996020@qq.com>
2021-01-02 18:40:20 +0800

    New train

--- history/02/single-channel-train/second/utils.py
+++ history/02/single-channel-train/second/utils.py
@@ -3,7 +3,7 @@ import numpy
 import os
 
 def save_loss_values(directory:str, epoch:int, all_loss:list):
-    with open(directory + str(epoch) + "_all", "w") as fp:
+    with open(os.path.join(directory, str(epoch) + "_all"), "w") as fp:
         for i in all_loss:
             fp.write(str(i) + '\n')
         fp.flush()
