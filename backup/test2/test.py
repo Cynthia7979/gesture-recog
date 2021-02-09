9c33e03025e8df6e438b0a89b052db3473027d26
Joss Lei <825996020@qq.com>
2021-02-09 10:33:51 +0800

    Test 2

---
+++ backup/test2/test.py
@@ -0,0 +1,26 @@
+import torch
+from train import VGG16
+from train import testLoader
+
+if "__main__" == __name__:
+    # use gpu1
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    vgg16 = VGG16(n_classes=3)
+    vgg16.load_state_dict(torch.load("cnn.pkl"))
+    vgg16.cuda()
+    # Test the model
+    vgg16.eval()
+    correct = 0
+    total = 0
+
+    for images, labels in testLoader:
+        images = images.cuda()
+        _, outputs = vgg16(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted.cpu() == labels).sum()
+        print(predicted, labels, correct, total)
+        print("avg acc: %f" % (100* correct/total))
+        
